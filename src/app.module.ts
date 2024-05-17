7d2d9471e0f8e3e9100f504f822cbd99ad153684
Corvax <vlvete1990@yandex.ru>
2024-05-17 19:09:58 +0300

    feature: add base controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthorController } from './author/author.controller';
+import { BookController } from './book/book.controller';
+import { GenreController } from './genre/genre.controller';
+import { GoalController } from './goal/goal.controller';
+import { PublishingController } from './publishing/publishing.controller';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AuthorController,
+    BookController,
+    GenreController,
+    GoalController,
+    PublishingController,
+    UserController,
+  ],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/publishing/publishing.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('publishing')
+export class PublishingController {}
