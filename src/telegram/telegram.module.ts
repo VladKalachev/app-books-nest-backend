8b2b939e9237599ada1c8aacedeb68b1f55bca3f
Corvax <vlvete1990@yandex.ru>
2024-05-31 23:47:22 +0300

    feature: init telegraf

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import * as LocalSession from 'telegraf-session-local';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTelegramConfig } from 'src/configs/telegram.config';
+
+const sessions = new LocalSession({ database: 'session_db.json' });
 
 @Module({
   controllers: [],
-  imports: [],
+  imports: [
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          middlewares: [sessions.middleware()],
+          ...getTelegramConfig(configService),
+        };
+      },
+    }),
+  ],
   providers: [TelegramService],
 })
 export class TelegramModule {}
